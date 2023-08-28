0f4c6da631fbf8bb7d07c4423fe8527a9eeed46a
Huilen Solis <xywvanz@gmail.com>
2023-08-27 23:05:55 -0300

    created not found page

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,5 +1,12 @@
+import notFoundSvg from '../../assets/404.svg'
+
 const NotFound = () => {
-    return <>NotFound</>
+    return (
+        <section className="w-screen h-[calc(100vh-80px)] flex flex-col items-center justify-center">
+            <h1 className='text-9xl text-slate-300'>404</h1>
+            <img src={notFoundSvg} alt="404 not found" className="w-96" />
+        </section>
+    )
 }
 
 export default NotFound
